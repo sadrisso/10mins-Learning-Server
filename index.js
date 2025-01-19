23b7f5b3c847ac7434abbfec256281d47d7ab323
sadrisso <sadriisso304@gmail.com>
2025-01-19 21:16:17 +0600

    create token and verify token

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
 
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 
 
@@ -34,6 +35,32 @@ async function run() {
         const studySessionsCollection = client.db("learningDB").collection("studySessions")
 
 
+        //jwt releted apis starts from here
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        const verifyToken = (req, res, next) => {
+            console.log("inside verify token : ", req.headers.authorization)
+
+            if (!req?.headers?.authorization) {
+                return res.status(401).send({ message: 'forbidded access' })
+            }
+            const token = req?.headers?.authorization.split(" ")[1]
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+        //jwt releted apis ends here
+
+
 
         //user releted apis starts
         app.post("/users", async (req, res) => {
@@ -48,7 +75,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get("/users", async (req, res) => {
+        app.get("/users", verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
@@ -56,7 +83,7 @@ async function run() {
         app.patch("/users/admin/:id", async (req, res) => {
             const id = req.params;
             const info = req.body;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set: {
                     role: info.role
@@ -71,7 +98,7 @@ async function run() {
 
 
         //tutor releted apis starts from here
-        app.get("/tutor", async (req, res) => {
+        app.get("/tutors", async (req, res) => {
             const filter = { role: "Tutor" }
             const result = await userCollection.find(filter).toArray()
             res.send(result)
